c325381937e9ec3b54ca4cccbd1551d2b6b7d6f4
ZebdaYacine <zebdaadam1996@gmail.com>
2023-03-10 17:42:02 +0100

    adding sqlite jdbc

--- src/doctorlite/BackEnd/DataBaseConnection.java
+++ src/doctorlite/BackEnd/DataBaseConnection.java
@@ -25,7 +25,7 @@ public class DataBaseConnection {
 
     public static Connection Connect() {
         try {
-            con = DriverManager.getConnection(url, user, Password);
+            con = DriverManager.getConnection(url);
             System.out.println("Connection with Data Base");
         } catch (Exception e) {
             System.out.println(e.getMessage() + " " + e.getStackTrace());
